47386326164fe3adaef0d1dabf54134195165bac
Mafe-SL <fernanda.mariastb@gmail.com>
2023-01-12 19:41:23 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+import './App.css';
+import BarraSuperior from './components/BarraSuperior';
+import Contador from './components/Contador';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <BarraSuperior/>
+     <Contador/>
+
+    </div>
+  );
+}
+
+export default App;
